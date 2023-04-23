cb351ecd250b56df4093cdc57bdd91ff396471d3
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-23 22:03:38 +0530

    char area basic style

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import UserInput from './UserInput'
+import ChatArea from './ChatArea'
 
 const Chat = () => {
   return (
     <div className='h-screen flex flex-col justify-between mx-8 py-4'>
-      <div>chat here</div>
+      <ChatArea />
       <UserInput />
     </div>
   )
