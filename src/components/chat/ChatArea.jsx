4c25e3166a0a7c6c9f2a34fb8b0b9a29d8555cc7
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-23 23:02:50 +0530

    chat samples

--- src/components/chat/ChatArea.jsx
+++ src/components/chat/ChatArea.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
 
 const ChatArea = () => {
-  return <div className='text-stone-100 h-full bg-slate-50'>ChatArea</div>
+  return (
+    <div className='h-full'>
+      <div className='chat chat-start'>
+        <div class='chat-image avatar'>
+          <div class='w-10 rounded-full'>
+            <img src='https://static.vecteezy.com/system/resources/previews/004/996/790/original/robot-chatbot-icon-sign-free-vector.jpg' />
+          </div>
+        </div>
+        <div className='chat-bubble chat-bubble-info'>
+          It's over Anakin, <br />I have the high ground.
+        </div>
+      </div>
+      <div className='chat chat-end'>
+        <div class='chat-image avatar'>
+          <div class='w-10 rounded-full'>
+            <img src='https://static.vecteezy.com/system/resources/previews/004/996/790/original/robot-chatbot-icon-sign-free-vector.jpg' />
+          </div>
+        </div>
+        <div className='chat-bubble  chat-bubble-primary'>
+          You underestimate my power!
+        </div>
+      </div>
+      <div className='chat chat-end'>
+        <div class='chat-image avatar'>
+          <div class='w-10 rounded-full'>
+            <img src='https://static.vecteezy.com/system/resources/previews/004/996/790/original/robot-chatbot-icon-sign-free-vector.jpg' />
+          </div>
+        </div>
+        <div className='chat-bubble  chat-bubble-primary'>
+          You underestimate my power!
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ChatArea
