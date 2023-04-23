dfb538f3c177f3f50df57945afc560f76ee20f08
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-23 20:37:33 +0530

    emotion comp

---
+++ src/components/emotion/Emotion.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Emotion = () => {
+  return (
+    <div className='bg-red-900 h-screen flex justify-center items-center'>
+      Emotion
+    </div>
+  )
+}
+
+export default Emotion
