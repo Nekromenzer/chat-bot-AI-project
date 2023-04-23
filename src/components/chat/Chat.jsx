34023df739bc42d656117cbabb41d9e6d72c2ca7
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-23 20:37:40 +0530

    chat page

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserInput from './UserInput'
+
+const Chat = () => {
+  return (
+    <div className='h-screen flex flex-col justify-between mx-8 py-4'>
+      <div>chat here</div>
+      <UserInput />
+    </div>
+  )
+}
+
+export default Chat
