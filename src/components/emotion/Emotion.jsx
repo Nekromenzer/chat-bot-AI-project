c3bf2b14456c1df74b2ebbcc5c08887708012f62
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-23 23:02:32 +0530

    remove bg

--- src/components/emotion/Emotion.jsx
+++ src/components/emotion/Emotion.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Emotion = () => {
   return (
-    <div className='bg-red-900 h-screen flex justify-center items-center'>
+    <div className='h-screen flex justify-center items-center'>
       Emotion
     </div>
   )
