df8c379ff9b469bb8468635af862dce89738de34
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-23 20:37:10 +0530

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css'
+import Chat from './components/chat/Chat'
+import Emotion from './components/emotion/Emotion'
 
 function App () {
-  return <div className='App'>App</div>
+  return (
+    <div className='bg-slate-950 h-screen flex flex-col md:flex-row'>
+      <div className='basis-full md:basis-1/4 md:block hidden'>
+        <Emotion />
+      </div>
+      <div className='basis-full md:basis-3/4'>
+        <Chat />
+      </div>
+    </div>
+  )
 }
 
 export default App
