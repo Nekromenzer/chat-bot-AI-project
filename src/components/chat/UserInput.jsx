8b45370c235490144aa5fb899025f786dbba11da
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-23 22:01:28 +0530

    user input fix

--- src/components/chat/UserInput.jsx
+++ src/components/chat/UserInput.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import sendIcon from '../../assets/send-icon.png'
 
-const UserInput = ({ value, onBlur, onChange }) => {
+const UserInput = ({ value, onBlur, onChange, onClick }) => {
   return (
-    <div>
+    <div className='flex items-center gap-2'>
       <input
         type='text'
         value={value}
         onBlur={onBlur}
         onChange={onChange}
-        className='block w-full rounded-md border-0 py-1.5 pl-7 px-8 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+        placeholder='say hi!'
+        className='block w-full rounded-md border-0 py-1.5 px-4 antialiased text-stone-100 bg-slate-700 tracking-wider slate-gray-300 placeholder:text-stone-400 text-lg focus:ring-1 focus:ring-slate-700 sm:text-sm sm:leading-6'
       />
+      <button
+        className='rounded-md border-0 text-stone-90 text-sm py-2 px-3 font-bold'
+        onClick={onClick}
+      >
+        <img src={sendIcon} alt='send icon' className='w-8' />
+      </button>
     </div>
   )
 }
