884bad701235c8d3e4398671a380ad1b2fe08213
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-24 22:18:21 +0530

    add robot

--- src/components/emotion/Emotion.jsx
+++ src/components/emotion/Emotion.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import robotHi from '../../assets/robot/robot.svg'
 
 const Emotion = () => {
   return (
     <div className='h-screen flex justify-center items-center'>
-      Emotion
+      <img src={robotHi} alt='robot' className='w-100 animate-pulse' />
     </div>
   )
 }
