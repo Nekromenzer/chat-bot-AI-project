81f38e10d8478780117a108f5f8eae14a845734b
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-24 19:39:17 +0530

    theme config and main theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter'
+    ]
+  },
   theme: {
     extend: {
       fontFamily: {
@@ -9,5 +42,5 @@ export default {
       }
     }
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')]
 }
