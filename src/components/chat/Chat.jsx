73500a068438c267149cbaf8451c977a58b03583
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-30 19:27:11 +0530

    chat conversation logic , array

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -5,20 +5,43 @@ import { communicateWithUser } from '../logic/bot'
 
 const Chat = () => {
   const [inputValue, setInputValue] = useState('')
+  const [conversation, setConversation] = useState([])
+
+  const makeConversation = (val, type) => {
+    const newObj = { msg: val, type }
+    setConversation(preArray => [...preArray, newObj])
+  }
 
   const callBack = val => {
+    // handle bot reply
     console.log(val)
+    makeConversation(val, 'bot')
   }
+
+  const clearInput = () => {
+    setTimeout(() => {
+      setInputValue('')
+    }, 200)
+  }
+
+  const getBotReply = () => {
+    makeConversation(inputValue, 'not-bot')
+    communicateWithUser(inputValue, callBack)
+    clearInput()
+  }
+
+  console.log(conversation)
+
   return (
     <div className='h-screen flex flex-col justify-between mx-3 md:mx-8 py-4 gap-2'>
-      <ChatArea />
+      <ChatArea conversation={conversation} />
       <UserInput
         value={inputValue}
         onChange={e => setInputValue(e.target.value)}
-        onClick={() => communicateWithUser(inputValue, callBack)}
+        onClick={() => getBotReply()}
         onKeyDown={e => {
           if (e.keyCode === 13) {
-            communicateWithUser(inputValue, callBack)
+            getBotReply()
           }
         }}
       />
