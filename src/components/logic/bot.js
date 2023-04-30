c80d7100f38a249a86295ad17eb33abbbc77cf7e
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-30 19:26:27 +0530

    fix logic

--- src/components/logic/bot.js
+++ src/components/logic/bot.js
@@ -1,6 +1,15 @@
+const hiArray = ['Hi', 'Hello', 'Howdy', 'Hello there']
+
+//  helper functions
+function getRandomValueFromArray (arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length)
+  return arr[randomIndex]
+}
+
 export const communicateWithUser = (userText, cb) => {
   if (userText === 'hi') {
-    return cb('hi machn kohomada')
+    return cb(getRandomValueFromArray(hiArray))
+  } else {
+    return cb('man dan nah yakoo')
   }
-  return 'man dan nah yakoo'
 }
