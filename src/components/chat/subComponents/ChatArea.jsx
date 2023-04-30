45116941fc81407ff003cc64086d7506024aaaed
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-30 19:41:55 +0530

    capitalize first letter

--- src/components/chat/subComponents/ChatArea.jsx
+++ src/components/chat/subComponents/ChatArea.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ChatBubble from '../chatBubble/ChatBubble'
 
 const ChatArea = ({ conversation }) => {
+  function capitalizeFirstLetter (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
   return (
     <div className='h-full max-h-[90vh] overflow-auto scroll-smooth pr-8 -mr-8 scrollbar-thin scrollbar-thumb-info-content scrollbar-track-gray-100'>
       {conversation.map((item, key) => (
-        <ChatBubble msg={item.msg} key={key} ai={item.type === 'bot'} />
+        <ChatBubble
+          msg={capitalizeFirstLetter(item.msg)}
+          key={key}
+          ai={item.type === 'bot'}
+        />
       ))}
     </div>
   )
