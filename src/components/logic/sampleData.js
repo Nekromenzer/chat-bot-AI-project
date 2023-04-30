29fe61da72dc03d6e05125ec0b80833715aa98f8
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-30 20:13:09 +0530

    add sample data

---
+++ src/components/logic/sampleData.js
@@ -0,0 +1,35 @@
+const hiArray = [
+  'Hi',
+  'Hello',
+  'Howdy',
+  'Hello there',
+  'Hey',
+  'What’s up',
+  'Good day',
+  'Hiya',
+  'Hey there',
+  'Hi-ya',
+  'Hola',
+]
+
+const educationHashMap = {
+    'course list': 'Here are the courses that we offer...',
+    'course details': 'The course details are as follows...',
+    'enrollment': 'Please provide your personal and enrollment details...',
+    'fees': 'The fees for the selected course are $5000.',
+    'scholarship': 'Based on your academic performance, you are eligible for a scholarship of $2000.',
+    'admission process': 'The admission process includes...',
+    'career opportunities': 'Our graduates have opportunities in various fields such as...',
+    'help': 'How can I assist you today?',
+    'greeting': 'Hello! How may I assist you?',
+    'goodbye': 'Thank you for considering our institute. Have a great day!',
+    'campus life': 'Our campus has various facilities such as a library, sports center, and cafeteria.',
+    'faculty': 'Our faculty members are highly qualified and experienced in their respective fields.',
+    'student organizations': 'We have various student organizations such as clubs, societies, and associations.',
+    'internships': 'We offer internship opportunities with top companies in the industry.',
+    'online courses': 'We also offer online courses for those who cannot attend classes in person.',
+    'course prerequisites': 'The prerequisites for the selected course are...',
+    // Add more intents and corresponding responses as needed
+  };
+
+export { hiArray }
