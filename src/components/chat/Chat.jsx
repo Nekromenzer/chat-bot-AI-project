3d0d9143f64acfdda712a6aad3ac36bfb436f915
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-30 18:13:14 +0530

    chat func

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserInput from './subComponents/UserInput'
 import ChatArea from './subComponents/ChatArea'
+import { communicateWithUser } from '../logic/bot'
 
 const Chat = () => {
+  const [inputValue, setInputValue] = useState('')
+
+  const callBack = val => {
+    console.log(val)
+  }
   return (
     <div className='h-screen flex flex-col justify-between mx-3 md:mx-8 py-4 gap-2'>
       <ChatArea />
-      <UserInput />
+      <UserInput
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+        onClick={() => communicateWithUser(inputValue, callBack)}
+        onKeyDown={e => {
+          if (e.keyCode === 13) {
+            communicateWithUser(inputValue, callBack)
+          }
+        }}
+      />
     </div>
   )
 }
