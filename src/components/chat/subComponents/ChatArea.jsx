bf6486e68f7b67754bd36977bd97234fa9d54f7a
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-04-30 20:12:51 +0530

    auto scroll to bottom

--- src/components/chat/subComponents/ChatArea.jsx
+++ src/components/chat/subComponents/ChatArea.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import ChatBubble from '../chatBubble/ChatBubble'
 
 const ChatArea = ({ conversation }) => {
+  const chatWindowRef = useRef(null)
+
   function capitalizeFirstLetter (str) {
     return str.charAt(0).toUpperCase() + str.slice(1)
   }
+
+  useEffect(() => {
+    // Scroll to the bottom of the chat window
+    chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight
+  }, [conversation])
   return (
-    <div className='h-full max-h-[90vh] overflow-auto scroll-smooth pr-8 -mr-8 scrollbar-thin scrollbar-thumb-info-content scrollbar-track-gray-100'>
+    <div
+      className='h-full max-h-[90vh] overflow-auto scroll-smooth pr-8 -mr-8 scrollbar-thin scrollbar-thumb-info-content scrollbar-track-gray-100'
+      ref={chatWindowRef}
+    >
       {conversation.map((item, key) => (
         <ChatBubble
           msg={capitalizeFirstLetter(item.msg)}
