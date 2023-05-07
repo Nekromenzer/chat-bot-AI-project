126c355a61e16bb6b662de250e3faa75c6298249
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-05-07 18:39:11 +0530

    bot logic

--- src/components/logic/bot.js
+++ src/components/logic/bot.js
@@ -1,6 +1,7 @@
-import { hiArray } from './sampleData'
+import { hiArray, educationHashMap } from './sampleData'
 
 //  helper functions
+// get random value from array provided
 function getRandomValueFromArray (arr) {
   const randomIndex = Math.floor(Math.random() * arr.length)
   return arr[randomIndex]
@@ -9,9 +10,17 @@ function getRandomValueFromArray (arr) {
 export const communicateWithUser = (userText, cb) => {
   //user input covert to lowercase
   const lowercasedUserText = userText.toLowerCase()
-
+  // static replies
   if (lowercasedUserText === 'hi') {
     return cb(getRandomValueFromArray(hiArray))
+  }
+  // get values from educationHashMap
+  if (
+    lowercasedUserText.includes('course') ||
+    lowercasedUserText.includes('courses')
+  ) {
+    const value = educationHashMap['courses']
+    return cb(value)
   } else {
     return cb('man dan nah yakoo')
   }
