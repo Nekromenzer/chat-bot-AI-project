b4db89f247a744bca6a2451f1647520494f695b8
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-05-13 16:56:40 +0530

    get local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Chat from './components/chat/Chat'
 import Emotion from './components/emotion/Emotion'
 import { educationHashMap } from './components/logic/sampleData'
+import useDependentEffect from './components/hooks/useDependentEffect'
 
 function App () {
   const [conversation, setConversation] = useState([])
   const [hashMapState, setHashMapState] = useState(educationHashMap)
+  const storedHashMap = localStorage.getItem('hashArray')
+  const storedConversation = localStorage.getItem('conversationHistory')
+
+  // useEffect(() => {
+  //   if (storedHashMap === null) {
+  //     localStorage.setItem('hashArray', JSON.stringify(hashMapState))
+  //   }
+  // }, [hashMapState])
+
+  // useEffect(() => {
+  //   if (storedConversation === null) {
+  //     localStorage.setItem('conversationHistory', JSON.stringify(conversation))
+  //   } else if (storedConversation.length === 0) {
+  //     console.log(storedConversation, 'storedConversation')
+  //   }
+  // }, [conversation])
+
+  useDependentEffect(() => {
+    if (conversation.length) {
+      localStorage.setItem('conversationHistory', JSON.stringify(conversation))
+    }
+  }, [conversation])
+
+  useEffect(() => {
+    if (storedHashMap !== null) {
+      setHashMapState(JSON.parse(storedHashMap))
+    }
+    if (storedConversation !== null) {
+      console.log(JSON.parse(storedConversation,"sadadasd"))
+      setConversation(JSON.parse(storedConversation))
+    }
+  }, [])
 
   return (
     <div className='bg-base-100 h-screen flex flex-col md:flex-row'>
