405c2191fbe51c14085427a750d149695c61c48a
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-05-16 02:51:41 +0530

    pass conversation

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App () {
   return (
     <div className='bg-base-100 h-screen flex flex-col md:flex-row'>
       <div className='basis-full md:basis-1/4 md:block hidden'>
-        <Emotion />
+        <Emotion conversation={conversation} />
       </div>
       <div className='basis-full md:basis-2/4'>
         <Chat
