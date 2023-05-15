a8e9cf44147fafeb547d079bc74af7dc5de17351
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-05-16 02:51:48 +0530

    basic emotions

--- src/components/emotion/Emotion.jsx
+++ src/components/emotion/Emotion.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import robotHi from '../../assets/robot/robot.svg'
+import robotHi from '../../assets/robot/robot.svg'
 import {
   SadRobot,
   LoveRobot,
@@ -13,10 +13,32 @@ import {
   NervoseRobot
 } from '../../assets/emotions'
 
-const Emotion = () => {
+const Emotion = ({ conversation }) => {
+  const getBotRepliesOnly =
+    conversation.length && conversation?.filter(obj => obj.type === 'bot')
+  const lastMsgObjConversation = getBotRepliesOnly[getBotRepliesOnly.length - 1]
+  const lastMsgEmotion = lastMsgObjConversation?.emotion || 0
+
+  const getEmotionRobot = () => {
+    if (lastMsgEmotion === 1) {
+      return SmileRobot
+    }
+    if (lastMsgEmotion === 5) {
+      return NervoseRobot
+    }
+    if (lastMsgEmotion === 6) {
+      return SurpriseRobot
+    }
+    return robotHi
+  }
+
   return (
     <div className='h-screen flex justify-center items-center'>
-      <img src={SadRobot} alt='robot' className='w-100 animate-pulse' />
+      <img
+        src={getEmotionRobot()}
+        alt='robot'
+        className='w-100 animate-pulse'
+      />
     </div>
   )
 }
