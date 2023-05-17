f790bc60b5492adf7c273e77df8a0fa8917d645c
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-05-18 01:29:13 +0530

    add new step code optimize

--- src/components/chat/chatBubble/ChatBubble.jsx
+++ src/components/chat/chatBubble/ChatBubble.jsx
@@ -8,10 +8,8 @@ const ChatBubble = ({
   isAvatar = true,
   isArray,
   msgType,
-  customText,
-  emotion
+  customText
 }) => {
-
   return (
     <div className={`chat ${ai ? 'chat-end' : 'chat-start'}`}>
       {isAvatar && (
@@ -26,76 +24,83 @@ const ChatBubble = ({
           ai ? 'chat-bubble-accent' : 'chat-bubble-secondary'
         }`}
       >
+        {customText && (
+          <div className='my-2 text-start font-mono text-base subpixel-antialiased'>
+            {customText}
+          </div>
+        )}
         {isArray(msg) ? (
           <>
             {msgType === 'table-simple' ? (
-              <>
-                {customText && (
-                  <div className='my-2 text-start font-mono text-base subpixel-antialiased'>
-                    {customText}
-                  </div>
-                )}
-                <div className='overflow-x-auto'>
-                  <table className='table w-full table-zebra table-compact subpixel-antialiased'>
-                    <tbody>
-                      {msg.map((item, idx) => (
-                        <tr key={idx} className='hover cursor-pointer'>
-                          <th className='text-neutral-content msg font-mono subpixel-antialiased'>
-                            {idx + 1}.
-                          </th>
-                          <th className='font-light msg font-mono subpixel-antialiased text-slate-200'>
-                            {item}
-                          </th>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </>
+              <div className='overflow-x-auto'>
+                <table className='table w-full table-zebra table-compact subpixel-antialiased'>
+                  <tbody>
+                    {msg.map((item, idx) => (
+                      <tr key={idx} className='hover cursor-pointer'>
+                        <th className='text-neutral-content msg font-mono subpixel-antialiased'>
+                          {idx + 1}.
+                        </th>
+                        <th
+                          className='font-light msg font-mono subpixel-antialiased text-slate-200'
+                          dangerouslySetInnerHTML={{ __html: item }}
+                        />
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             ) : msgType === 'stats' ? (
-              <>
-                {customText && (
-                  <div className='my-2 text-start font-mono text-base subpixel-antialiased'>
-                    {customText}
-                  </div>
-                )}
-                <div className='stats stats-vertical shadow'>
-                  {msg.map((item, idx) => (
-                    <div key={idx} className='stat'>
-                      <div className='stat-title font-mono text-neutral-content subpixel-antialiased'>
-                        {item.key}
-                      </div>
-                      <div className='text-primary font-bold font-mono tracking-wide subpixel-antialiased'>
-                        {item.value}
-                      </div>
-                      <div className='stat-figure text-primary'>
-                        <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          fill='none'
-                          viewBox='0 0 24 24'
-                          className='inline-block w-8 h-8 stroke-current'
-                        >
-                          <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            strokeWidth='2'
-                            d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                          ></path>
-                        </svg>
-                      </div>
-                      {/* <div className='stat-desc'>0{idx + 1}</div> */}
+              <div className='stats stats-vertical shadow'>
+                {msg.map((item, idx) => (
+                  <div key={idx} className='stat'>
+                    <div className='stat-title font-mono text-neutral-content subpixel-antialiased'>
+                      {item.key}
                     </div>
-                  ))}
-                </div>
-              </>
-            ) : msgType === 'unordered' &&(
+                    <div className='text-primary font-bold font-mono tracking-wide subpixel-antialiased'>
+                      {item.value}
+                    </div>
+                    <div className='stat-figure text-primary'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        className='inline-block w-8 h-8 stroke-current'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          strokeWidth='2'
+                          d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+                        ></path>
+                      </svg>
+                    </div>
+                    {/* <div className='stat-desc'>0{idx + 1}</div> */}
+                  </div>
+                ))}
+              </div>
+            ) : msgType === 'unordered' ? (
               <ul className='bg-base-300 p-2 rounded-md'>
                 {msg.map((item, idx) => (
-                  <li key={idx} className='font-mono subpixel-antialiased text-slate-200'>
+                  <li
+                    key={idx}
+                    className='font-mono subpixel-antialiased text-slate-200'
+                  >
                     👉 {item}
                   </li>
                 ))}
               </ul>
+            ) : (
+              msgType === 'steps' && (
+                <ul className='steps steps-vertical'>
+                  {msg.map((item, idx) => (
+                    <li
+                      key={idx}
+                      className='font-mono subpixel-antialiased step min-h-2'
+                      dangerouslySetInnerHTML={{ __html: item }}
+                    />
+                  ))}
+                </ul>
+              )
             )}
           </>
         ) : (
@@ -110,7 +115,6 @@ const ChatBubble = ({
               }`}
               dangerouslySetInnerHTML={{ __html: msg }}
             />
-            {/* {emotion === 2 ? <div>😋</div> : ''} */}
           </div>
         )}
       </div>
